c16630c7564896191bca52eefb9ea24812e61b00
Chloe Court <crc.courtois@gmail.com>
2023-03-28 23:27:21 +0200

    created chat component

---
+++ components/home/chat.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+export default function Chat() {
+  return (
+    <div className="px-3">
+      <div className="flex-grow overflow-y-auto px-3">
+        {/* <!-- Chat messages --> */}
+        <div className=" flex flex-col gap-4 py-4">
+          <div className="flex justify-start">
+            {/* <div className="max-w-[80%] rounded-lg bg-gray-100 px-4 py-2">
+              <p className="text-sm text-gray-900">Hey, how are you?</p>
+            </div> */}
+          </div>
+          <div className="flex justify-end">
+            <div className="max-w-[80%] rounded-lg bg-blue-500 px-4 py-2">
+              <p className="text-sm text-white">
+                Hey, when is my package arriving?
+              </p>
+            </div>
+          </div>
+          <div className="flex justify-start">
+            <div className="max-w-[80%] rounded-lg bg-gray-100 px-4 py-2">
+              <p className="text-sm text-gray-900">Hi Sam, Let me check.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex h-16 items-center justify-center">
+        {/* <!-- Chat input --> */}
+        <input
+          type="text"
+          className="mr-4 w-full max-w-lg rounded-lg border border-gray-300 py-2 px-4"
+          placeholder="It arrives in two days"
+        />
+        <button className="rounded bg-blue-500 py-2 px-4 font-bold text-white hover:bg-blue-700">
+          Send
+        </button>
+      </div>
+    </div>
+  );
+}
