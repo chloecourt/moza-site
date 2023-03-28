1055dbe96584a086e0b14c4eb21a1ea1634b405a
Chloe Court <crc.courtois@gmail.com>
2023-03-28 23:26:10 +0200

    changed viewbox for instagram svg component

--- components/shared/icons/Instagram.tsx
+++ components/shared/icons/Instagram.tsx
@@ -2,7 +2,7 @@ export default function Instagram({ className }: { className?: string }) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 150 150"
+      viewBox="0 0 130 130"
       className={className}
     >
       <defs>
