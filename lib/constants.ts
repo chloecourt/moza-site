e0c4385dd45d9a4fb5ba3e8e0361aeb8c8c8da7b
Chloe Court <crc.courtois@gmail.com>
2023-03-28 14:52:13 +0200

    unknown issue occuring if opacity is not set to 0.9, will not show text and social media icons around main h1 if at 1 like it was previously

--- lib/constants.ts
+++ lib/constants.ts
@@ -3,10 +3,10 @@ export const FADE_IN_ANIMATION_SETTINGS = {
   animate: { opacity: 1 },
   transition: { duration: 0.2 },
 };
-
+// TODO: fix the opacity at 0.9 on show -> previously at one but all of the sudden disappeared unless lowered to 0.9...
 export const FADE_DOWN_ANIMATION_VARIANTS = {
   hidden: { opacity: 0, y: -10 },
-  show: { opacity: 1, y: 0, transition: { type: "spring" } },
+  show: { opacity: 0.9, y: 0, transition: { type: "spring" } },
 };
 
 export const FADE_UP_ANIMATION_VARIANTS = {
