32c67c7ca588efc05a01807a26807055c6156dc3
Chloe Court <crc.courtois@gmail.com>
2023-03-28 23:25:14 +0200

    added export for chat component

--- components/home/index.tsx
+++ components/home/index.tsx
@@ -1,3 +1,4 @@
 export { default as LineChart } from "./LineChart";
 export { default as SocialMedia } from "./social-media";
 export { default as ShoeProduct } from "./shoe-product";
+export { default as Chat } from "./chat";
