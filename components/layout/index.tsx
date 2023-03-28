e90328a6eccbf269c7210d512a6b58ad60349924
Chloe Court <crc.courtois@gmail.com>
2023-03-28 14:53:13 +0200

    removed unnecessary auth

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,13 +1,10 @@
 import MozaLogo from "@/components/layout/logoText";
 import { FADE_IN_ANIMATION_SETTINGS } from "@/lib/constants";
 import { AnimatePresence, motion } from "framer-motion";
-import { useSession } from "next-auth/react";
-import Image from "next/image";
 import Link from "next/link";
 import { ReactNode } from "react";
 import useScroll from "@/lib/hooks/use-scroll";
 import Meta from "./meta";
-import { useSignInModal } from "./sign-in-modal";
 import UserDropdown from "./user-dropdown";
 
 export default function Layout({
@@ -21,14 +18,11 @@ export default function Layout({
   };
   children: ReactNode;
 }) {
-  const { data: session, status } = useSession();
-  const { SignInModal, setShowSignInModal } = useSignInModal();
   const scrolled = useScroll(50);
 
   return (
     <>
       <Meta {...meta} />
-      <SignInModal />
       <div className="fixed h-screen w-full bg-gradient-to-br from-orange-50 via-white to-red-50" />
 
       <div
